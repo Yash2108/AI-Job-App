07624e213ebf85a58ac9bf434f84e5b4139d706c
Yash Jain <1999yashjain@gmail.com>
2025-08-07 18:38:07 -0400

    Can now tailor resume bullet points using inference from Llama 3

--- resume_tailor.py
+++ resume_tailor.py
@@ -1,29 +1,74 @@
-from preprocess_utils import read_from_txt
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from preprocess_utils import *
+from huggingface_hub import InferenceClient
+import json
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+load_dotenv()
 
+HF_TOKEN_READ = os.getenv("HF_TOKEN_READ")
+# HF_TOKEN_WRITE = os.getenv("HF_TOKEN_WRITE")
 
-tokenizer = AutoTokenizer.from_pretrained("nakamoto-yama/t5-resume-generation")
-model = AutoModelForSeq2SeqLM.from_pretrained("nakamoto-yama/t5-resume-generation")
+SYSTEM_PROMPT = '''You are an expert at tailoring resumes. Given a job description and a resume bullet point, make the necessary changes to make sure the bullet point caters to some part of the job description.
 
+Input format:
+Description:
+<job description>
 
-bullet_point = '''Boosted team onboarding efficiency by 50% by building an in-house ATS using Python, \
-SpaCy and SciKit-Learn to parse resumes and recommend candidates to teams via customized ML-based matching system'''
-job_description_path = 'job_description.txt'
+Resume Bullet point:
+<point>
 
-extracted_jd = read_from_txt(job_description_path)
+Output:
+Write down 1 bullet point with atmost 100 characters.
 
-
-prompt = f'''
-You are an expert resume writer. I will share with you a bullet point and a job description for which I am applying. 
-Tailor this bullet point to fit the job.
-Here is the bullet point:
-{bullet_point}
-
-Here is the job description:
-{extracted_jd}
+Do not include any other text.
 '''
-input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids
-output_ids = model.generate(input_ids, max_length=512)
 
-generated_point = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-print(generated_point)
+
+def format_chat(system_prompt, user_input):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_input}
+    ]
+
+def inference_llama_3b(user_input):
+    client = InferenceClient(
+        provider="novita",
+        api_key=HF_TOKEN_READ,
+    )
+    messages = format_chat(SYSTEM_PROMPT, user_input)
+
+    completion = client.chat.completions.create(
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        messages=messages,
+        max_tokens=1024,
+        n=3
+    )
+    output = [choice.message.content for choice in completion.choices ]
+    return output
+
+def tailor_bullet_point(resume_bullet_point, extracted_jd):
+    input_text = f'''
+    Description:
+    {extracted_jd}
+
+    Resume Bullet point:
+    {resume_bullet_point}
+    '''
+    output = inference_llama_3b(input_text)
+
+    return output
+
+
+if __name__ == "__main__":
+
+    resume_path = 'resume.pdf'
+    job_description_path = 'job_description.txt'
+
+    resume_bullet_point = 'Building a personalized recommender system using Python (Pandas, Scikit-learn) and integrating it into a fullstack application with a Spring Boot backend and Angular frontend'
+    extracted_jd = read_from_txt(job_description_path)
+
+    options = tailor_bullet_point(resume_bullet_point, extracted_jd)
+
+    for option in options:
+        print(option)
